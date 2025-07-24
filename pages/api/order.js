9ca43a9318ddc21a8125917a3ccb9960aea7053e
BuchtaSolutions <simonbuchta2005@gmail.com>
2025-07-24 12:35:03 +0200

    order ep

--- pages/api/order.js
+++ pages/api/order.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const gasUrl = "https://script.google.com/macros/s/AKfycbyZ_ZYkDlW5wFx6sJZaqVayISj4Ouc3qu-lBrwADJGdy0VTOkuwVATHX17lzJ8CZEgywg/exec";
+    const gasUrl = "https://script.google.com/macros/s/AKfycbzE77IhsH3ukmu66w4_FP-ZlNfH6rC86RkfOJ7879M85oJ_58xCl0J9fV6ovz0JbjnUaQ/exec";
     const gasRes = await fetch(gasUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
